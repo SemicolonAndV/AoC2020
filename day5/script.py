bdd99f436f985caa2e1c1881f30cde832e8e7513
Kuba <kubapaj00@gmail.com>
2020-12-05 13:18:06 +0100

    fixed text formatting

--- day5/script.py
+++ day5/script.py
@@ -1,7 +1,7 @@
 conversion= ''.maketrans('FBRL', '0110')
 f = lambda x: x[0] * 8 + x[1]
 with open('day5/input.txt', 'r') as file:
-    data = ([f([int(x.strip()[:7].translate(conversion), 2), int(x.strip()[7:].translate(conversion), 2)]) for x in file.readlines()])
+    data = [f([int(x.strip()[:7].translate(conversion), 2), int(x.strip()[7:].translate(conversion), 2)]) for x in file.readlines()]
 empty_seats = {x for x in range(min(data), max(data))} - set(data)
 print(f'Result 1: {max(data)}')
-print(f'Result 2: {empty_seats}')
+print(f'Result 2: {list(empty_seats)[0]}')
