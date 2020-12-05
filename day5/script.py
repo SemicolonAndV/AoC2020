08566b8663ad3a0762ba37a5704f53e594c52fb9
Kuba <kubapaj00@gmail.com>
2020-12-05 10:08:04 +0100

    Solved day 5

---
+++ day5/script.py
@@ -0,0 +1,7 @@
+conversion= ''.maketrans('FBRL', '0110')
+f = lambda x: x[0] * 8 + x[1]
+with open('day5/input.txt', 'r') as file:
+    data = ([f([int(x.strip()[:7].translate(conversion), 2), int(x.strip()[7:].translate(conversion), 2)]) for x in file.readlines()])
+empty_seats = {x for x in range(96, 987)} - set(data)
+print(f'Result 1: {max(data)}')
+print(f'Result 2: {empty_seats}')
