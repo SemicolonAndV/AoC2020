87fb50a80764506730c2bea4085f91cbc04cba66
Kuba <kubapaj00@gmail.com>
2020-12-16 00:41:03 +0100

    fixed script to display result correctly

--- day15/script.py
+++ day15/script.py
@@ -4,19 +4,19 @@ numbers = {}
 for i, x in enumerate(data, start=1):
     numbers[x] = i
     
-def find_number(turn):
-    index = len(data) + 1
-    currnum = 0
-    while index < turn:
-        if currnum not in numbers:
-            numbers[currnum] = index
-            currnum = 0
-            index += 1
-        else:
-            lastnum = numbers[currnum]
-            numbers[currnum] = index
-            currnum = index - lastnum
-            index += 1
-    return currnum
+index = len(data) + 1
+currnum = 0
+while index < 30000000:
+    if currnum not in numbers:
+        numbers[currnum] = index
+        currnum = 0
+        index += 1
+    else:
+        lastnum = numbers[currnum]
+        numbers[currnum] = index
+        currnum = index - lastnum
+        index += 1
+    if index == 2020:
+        res1 = currnum
 
-print(f"Result 1: {find_number(2020)}\nResult 2: {find_number(30000000)}")
+print(f"Result 1: {res1}\nResult 2: {currnum}")
